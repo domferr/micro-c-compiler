20d1740e90ffbe8a56d5e84ea483358c1e57b120
Domenico Ferraro <ferraro.domenico125@gmail.com>
2022-10-25 23:17:37 +0200

    Print 3 lines before printing the error

--- microc-parser/bin/microcc.ml
+++ microc-parser/bin/microcc.ml
@@ -1,9 +1,29 @@
 open Microc
 
-let print_error outchan (pos :Location.lexeme_pos) msg = 
+let print_error file_channel outchan (pos :Location.lexeme_pos) msg =
+	let _ = seek_in file_channel 0 in
+	let rec skip_lines channel lines =
+		if lines <= 1 
+		then () 
+		else let _ = input_line channel in skip_lines channel (lines-1)
+	in
+	let rec read_lines channel lines acc = 
+		if lines <= 0
+		then acc
+		else let last = input_line channel 
+			in let new_acc = Printf.sprintf "%s%s\n" acc last 
+				in read_lines channel (lines-1) new_acc
+	in
+	let error_line = pos.Location.line in
+	let max_number_of_lines = 3 in
+	let number_of_lines = if error_line - max_number_of_lines > 0 then error_line - max_number_of_lines else 1 in
+	let first_line = error_line - number_of_lines in
+	let _ = skip_lines file_channel first_line in
+	let file_sourcecode_line = read_lines file_channel number_of_lines "" in
 	let left_spaces = String.make (pos.Location.start_column - 1) ' ' in 
-	let error_indication = String.make (pos.Location.end_column - pos.Location.start_column + 1) '^' in 
-	Printf.fprintf outchan "\n%s%s\n\027[1;31mError:\027[0m line %d, position %d\n       %s\n" left_spaces error_indication pos.Location.line pos.Location.start_column msg
+	let error_arrows = String.make (pos.Location.end_column - pos.Location.start_column + 1) '^' in
+	Printf.fprintf outchan "%s%s%s\n\027[1;31mError:\027[0m line %d, position %d\n       %s\n" 
+	file_sourcecode_line left_spaces error_arrows error_line pos.Location.start_column msg
 
 let () =
 	let input_channel =
@@ -15,6 +35,7 @@ let () =
   	let lexbuf = Lexing.from_channel ~with_positions:true input_channel in
   	try
     	let result = Microc.Parsing.parse Microc.Scanner.next_token lexbuf in
-      	Printf.fprintf stdout "Show: %s\n" (Microc.Ast.show_program result)
+      	Printf.fprintf stdout "\n%s\n" (Microc.Ast.show_program result)
   	with
-    	| Microc.Scanner.Lexing_error (pos, msg) -> print_error stderr pos msg
+    	| Microc.Scanner.Lexing_error (pos, msg) -> print_error input_channel stdout pos msg
+	
--- microc-parser/lib/scanner.mll
+++ microc-parser/lib/scanner.mll
@@ -4,8 +4,7 @@
     (* Auxiliary definitions *)
     exception Lexing_error of Location.lexeme_pos * string
 
-	(*open Printf
-	let create_hashtable size init =
+	(* let create_hashtable size init =
 		let tbl = Hashtbl.create size in
 		List.iter (fun (key, data) -> Hashtbl.add tbl key data) init;
 		tbl
@@ -13,38 +12,25 @@
 	let keywords_table =
 		create_hashtable 1 [
 			("if", IF)
-		]
-
-	let string_of_token = function
-		| PLUS   -> "PLUS"
-		| IF     -> "IF"
-		| ID(s)  -> Printf.sprintf "ID(%s)" s
-		| NUM(i) -> Printf.sprintf "INT_NUM(%d)" i
-		| EOF    -> "eof"
-	
-	let rec iterate scanner =
-		match scanner () with
-		| EOF -> ()
-		| tok -> Printf.printf "%s\n" (string_of_token tok); iterate scanner
-	*)
+		] *)
 }
 
+let newline = '\n' | '\r' '\n'
 let letter = ['a'-'z' 'A'-'Z']
 let digit = ['0' - '9']
 let identifier = letter (letter | digit | '_')*
 
 (* Scanner specification *)
 rule next_token = parse
-	  [' ' '\t' '\n' '\r']+   { next_token lexbuf }	(* ignore and skip whitespace *)
+	  [' ' '\t']+   	{ next_token lexbuf }	(* ignore and skip whitespace *)
+	| newline			{ Lexing.new_line lexbuf; next_token lexbuf }
 	| digit+ as lit 	{ INT(int_of_string lit) }
-	(*| identifier as word{ try					(* identifier or keyword *)
+	(* | identifier as word{ try					(* identifier or keyword *)
 						  let token = Hashtbl.find keywords_table word in
-							printf "keyword: %s\n" word;
 							token
 						  with Not_found ->
-							printf "identifier: %s\n" word;
 							ID word
-						}*)
+						} *)
 	| '+'            	{ ADD }
 	| '-'            	{ SUB }
 	| '*'            	{ MULT }
