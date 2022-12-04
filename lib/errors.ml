11f0d5736c0465d5354cf96058dde7364a7004e2
Domenico Ferraro <ferraro.domenico125@gmail.com>
2022-12-04 23:14:35 +0100

    Print vertical lines when the error is multiline

--- lib/errors.ml
+++ lib/errors.ml
@@ -2,52 +2,58 @@ open Location
 
 type error_location = SingleLine of Location.lexeme_pos | Multiline of Location.code_pos
 
-let print_error_lines error_channel sourcecode_channel from_line to_line =
+let print_sourcelines outchan sourcefile_chan start_line error_lines add_arrows =
+	let pre_line = if add_arrows then "  " else "" in
+	let pre_error_line = if add_arrows then "> " else "" in
 	try
-		for _ = 1 to from_line-1 do
-			ignore (input_line sourcecode_channel)
+		for _ = 0 to start_line - 1 do
+			ignore (input_line sourcefile_chan)
 		done;
-		for _ = from_line to to_line do
-			Printf.fprintf error_channel "%s\n" (input_line sourcecode_channel)
+		for _ = start_line to error_lines.start_line - 2 do
+			Printf.fprintf outchan "%s%s\n" pre_line (input_line sourcefile_chan)
+		done;
+		for _ = error_lines.start_line to error_lines.end_line do
+			Printf.fprintf outchan "\027[1;31m%s\027[0m%s\n" pre_error_line (input_line sourcefile_chan)
 		done
 	with _ -> ()
 
 let max_number_of_lines = 6 (* How many lines of the sourcecode to print before printing the error *)
 
-let get_start_line errorline =
-	if (errorline - max_number_of_lines + 1) <= 0 then 1
-	else errorline - max_number_of_lines + 1
+let print_arrows outchan start_column end_column = 
+	(* Create a string with number of spaces from the beginning of the line to the error column *)
+	let space_before_arrows = String.make (start_column - 1) ' ' in
+	(* Create a string with number of '^' arrows from the error's beginning column to the error's end column *)
+	let arrows_quantity = if start_column > end_column then 1 
+												else end_column - start_column + 1 in
+	let error_arrows = String.make arrows_quantity '^' in
+	Printf.fprintf outchan "%s\027[1;31m%s\027[0m\n" space_before_arrows error_arrows
 
 let report_error header_str file_channel outchan error_loc msg =
 	(* Lines of the source code that will be printed before printing the error message *)
 	let error_lines = match error_loc with
 			SingleLine(lex_pos) -> {
-				start_line 		= get_start_line lex_pos.line;
+				start_line 		= lex_pos.line;
 				start_column 	= lex_pos.start_column;
 				end_line 			= lex_pos.line;
 				end_column 		= lex_pos.end_column;
 			}
-		| Multiline(code_pos) -> {
-				start_line 		= get_start_line code_pos.start_line;
-				start_column 	= code_pos.start_column;
-				end_line 			= code_pos.end_line;
-				end_column 		= code_pos.end_column;
-			}
+		| Multiline(code_pos) -> code_pos
+	in
+	let is_multline = match error_loc with
+			SingleLine(_) -> false
+		| Multiline(_) -> true
 	in
 	(* Start reading the channel again from the beginning *)
 	seek_in file_channel 0;
-
+	
 	(* Finally print error lines *)
-	print_error_lines outchan file_channel error_lines.start_line error_lines.end_line;
+	let start_line = if (error_lines.start_line - max_number_of_lines + 1) < 0 then 0
+									 else error_lines.start_line - max_number_of_lines + 1 in
+	print_sourcelines outchan file_channel start_line error_lines is_multline;
 
 	(* Print error arrows *)
-	(* Create a string with number of spaces from the beginning of the line to the error column *)
-	let space_before_arrows = String.make (error_lines.start_column - 1) ' ' in
-	(* Create a string with number of '^' arrows from the error's beginning column to the error's end column *)
-	let arrows_quantity = if error_lines.start_column > error_lines.end_column then 1 
-												else error_lines.end_column - error_lines.start_column + 1 in
-	let error_arrows = String.make arrows_quantity '^' in
-	Printf.fprintf outchan "%s\027[1;31m%s\027[0m\n" space_before_arrows error_arrows;
+	if is_multline then Printf.fprintf outchan "\n" else
+	print_arrows outchan error_lines.start_column error_lines.end_column;
 
 	(* Print error location info and header *)
 	if error_lines.start_line == error_lines.end_line then
--- test/semantic_errors/invalid_main_def2.mc
+++ test/semantic_errors/invalid_main_def2.mc
@@ -1,3 +1,11 @@
+int global1;
+int test(bool b, int c) {
+    return 10 + b;
+}
+
 int main(int a) {
-    
+    bool b;
+    b = false;
+    int a[];
+    return a;
 }
